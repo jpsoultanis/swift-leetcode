123d897f673df5f9fa286bf24d08564528aaf832
Joe Soultanis <jpsoultanis@gmail.com>
2021-03-14 16:17:25 -0700

    Update best-time-to-buy-and-sell-stock.swift

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.swift
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.swift
@@ -1,13 +1,12 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-        var maxProfit = -1
+        var maxProfit = 0
         var maxPrice = -1
         
         for price in prices.reversed() {
             if price > maxPrice {
                 maxPrice = price
-            }
-            if maxPrice - price > maxProfit {
+            } else if maxPrice - price > maxProfit {
                 maxProfit = maxPrice - price
             }
         }
