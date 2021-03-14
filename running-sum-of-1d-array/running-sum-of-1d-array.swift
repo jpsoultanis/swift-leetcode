db0c48903ca75ffa61cf80567068ae008e35aa12
Joe Soultanis <jpsoultanis@gmail.com>
2021-03-14 16:33:20 -0700

    Update running-sum-of-1d-array.swift

--- running-sum-of-1d-array/running-sum-of-1d-array.swift
+++ running-sum-of-1d-array/running-sum-of-1d-array.swift
@@ -1,7 +1,7 @@
 class Solution {
     func runningSum(_ nums: [Int]) -> [Int] {
         return nums.reduce([]) { (sumsArray: [Int], num: Int) in
-            guard let last = sumsArray.last else { return [num] }
+            guard let last = sumsArray.last else { return [num] } // should only return for first element
             return sumsArray + [last + num]
         }
     }
