a060d767f21ca4fcdb3f7b00e0ba92f85ec52233
Joe Soultanis <jpsoultanis@gmail.com>
2021-03-14 17:10:05 -0700

    Update find-peak-element.swift

--- find-peak-element/find-peak-element.swift
+++ find-peak-element/find-peak-element.swift
@@ -1,7 +1,5 @@
 class Solution {
-    func findPeakElement(_ nums: [Int]) -> Int {
-        guard nums.count > 1 else { return 0 }
-        
+    func findPeakElement(_ nums: [Int]) -> Int {        
         let lastIndex = nums.count - 1
                 
         for curIndex in (0..<lastIndex) {
