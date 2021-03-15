86b826f41562da99c3909fcb98a4dbb86765c416
Joe Soultanis <jpsoultanis@gmail.com>
2021-03-14 17:52:18 -0700

    Time: 252 ms (37.41%), Space: 16.5 MB (14.29%) - LeetHub

---
+++ valid-palindrome-ii/valid-palindrome-ii.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func validPalindrome(_ s: String) -> Bool {
+        let s = Array(s)
+        var left = 0
+        var right = s.count - 1
+        
+        var hasIgnoredChar = false
+        
+        while left < right {
+            if s[left] == s[right] {
+                left += 1
+                right -= 1
+            } else {
+                // found a mismatch, try to skip the left or right char
+                let canDeleteLeft = Array(s[left+1...right]).isPalindrome()
+                let canDeleteRight = Array(s[left..<right]).isPalindrome()
+                return canDeleteLeft || canDeleteRight
+            }
+        }
+        
+        // if we get to here, the pointers have crossed and we can build a valid palindrone
+        return true
+    }
+}
+
+extension Array where Element == Character {
+    func isPalindrome() -> Bool {
+        return self == self.reversed()
+    }
+}
